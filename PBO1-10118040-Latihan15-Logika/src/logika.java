7e719b654d67fe5ff5bc170f30f7cde065aea888
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-09-26 13:23:15 +0700

    Add files via upload

---
+++ PBO1-10118040-Latihan15-Logika/src/logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Moch Yudha Rusdian
+ * Kelas     : PBO1
+ * NIM       : 10118040
+ * Deskripsi : Penggunaan operator logika
+ */
+public class logika {
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "+ (a && b));
+        System.out.println("a || b = "+ (a || b));
+        System.out.println("!a = "+ !a);
+        System.out.println("!(a && b) = " + !(a &&b));
+    }
+}
